4fa853bed8f586c75c3eb9c58320908f4de0e8bc
Vaibhav Sonawane <vaibhavsonawane2345@gmail.com>
2025-04-02 00:36:37 +0530

    add story in data.js

--- src/data/data.js
+++ src/data/data.js
@@ -9,6 +9,7 @@ export const booksData = [
     author: "F. Scott Fitzgerald",
     desc: "A classic novel of the Jazz Age, The Great Gatsby tells the story of Jay Gatsby's unrequited love for Daisy Buchanan.",
     image: img1,
+    story: "In my younger and more vulnerable years my father gave me some advice that I’ve been turning over in my mind ever since. “Whenever you feel like criticizing any one,” he told me, “just remember that all the people in this world haven’t had the advantages that you’ve had.”",
   },
   {
     id: 2,
@@ -16,6 +17,7 @@ export const booksData = [
     author: "Harper Lee",
     desc: "A gripping tale of racial injustice and moral growth in the Deep South, narrated by young Scout Finch.",
     image: img2,
+    story:"story: 'When he was nearly thirteen, my brother Jem got his arm badly broken at the elbow.'",
   },
   {
     id: 3,
@@ -23,6 +25,7 @@ export const booksData = [
     author: "George Orwell",
     desc: "A dystopian novel set in a totalitarian regime, exploring themes of surveillance, censorship, and individuality.",
     image: img3,
+    story: "It was a bright cold day in April, and the clocks were striking thirteen.",
   },
   {
     id: 4,
@@ -30,5 +33,6 @@ export const booksData = [
     author: "Jane Austen",
     desc: "A romantic novel that critiques the British landed gentry at the end of the 18th century, focusing on Elizabeth Bennet and Mr. Darcy.",
     image: img1,
+    story:"story..."
   },
 ];
