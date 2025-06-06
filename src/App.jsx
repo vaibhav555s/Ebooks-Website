e192a76f132acca3fbecd610fa6c5f7730de2c79
Vaibhav Sonawane <vaibhavsonawane2345@gmail.com>
2025-06-06 16:24:06 +0530

    Added new routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage"; // <-- if you placed the v0 layout here
 import StoryReader4 from "./components/StoryReader4";
 import AboutPage from "./components/AboutPage";
+import Trending from "./components/pages/Trending";
+import Feedback from "./components/pages/Feedback";
 
 export default function App() {
   return (
@@ -10,6 +12,8 @@ export default function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/story/:id" element={<StoryReader4 />} />
         <Route path="/about" element={<AboutPage />} />
+        <Route path="/trending" element={<Trending />} />
+        <Route path="/feedback" element={<Feedback />} />
       </Routes>
     </Router>
   );
