480a60712908c4528019b91d14b7436a3708efd6
Vaibhav Sonawane <vaibhavsonawane2345@gmail.com>
2025-06-06 16:25:11 +0530

    Separate trending books section and feedback

---
+++ src/components/pages/Feedback.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar3 from '../Navbar3'
+import FeedbackSection from '../FeedbackSection'
+import Footer from '../Footer'
+
+const Feedback = () => {
+  return (
+    <div>
+      <Navbar3 />
+      <FeedbackSection />
+        <Footer />
+    </div>
+  )
+}
+
+export default Feedback
---
+++ src/components/pages/Trending.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../Navbar3'
+import BooksSection2 from '../BooksSection2'
+import Footer from '../Footer'
+const Trending = () => {
+  return (
+    <div>
+        <Navbar />
+      <BooksSection2 />
+      <Footer />
+    </div>
+  )
+}
+
+export default Trending
