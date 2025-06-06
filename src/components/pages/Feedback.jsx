2c82374ad194bad9e074caf58f50e3afc64a7332
Vaibhav Sonawane <vaibhavsonawane2345@gmail.com>
2025-06-06 17:51:09 +0530

    Minor scroll issue fixed

--- src/components/pages/Feedback.jsx
+++ src/components/pages/Feedback.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 import Navbar3 from '../Navbar3'
 import FeedbackSection from '../FeedbackSection'
 import Footer from '../Footer'
+import { useEffect } from "react";
 
 const Feedback = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+  
   return (
     <div>
       <Navbar3 />
--- src/components/pages/Trending.jsx
+++ src/components/pages/Trending.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 import Navbar from '../Navbar3'
 import BooksSection2 from '../BooksSection2'
 import Footer from '../Footer'
+import { useEffect } from "react";
+
 const Trending = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <div>
         <Navbar />
