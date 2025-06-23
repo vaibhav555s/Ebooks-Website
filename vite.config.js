2723271ac2585e748fac067da03c6c15403cecdc
Vaibhav Sonawane <vaibhavsonawane2345@gmail.com>
2025-06-24 00:01:12 +0530

    Bug Fixes

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-// import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
   server: {
